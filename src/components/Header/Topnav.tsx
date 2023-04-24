9669900864ee80172484a1a68eeb0ffdcbb3f8d3
luskoeqk <lusy2017@gmail.com>
2023-04-24 13:45:16 +0300

    topnav adj

--- src/components/Header/Topnav.tsx
+++ src/components/Header/Topnav.tsx
@@ -1,16 +1,45 @@
-
+import Image from "next/image"
 import stlyes from "./Topnav.module.scss"
+
+// icons
+import FacebookIcon from "../../../public/icons/facebook.svg"
+import InstagramIcon from "../../../public/icons/instagram.svg"
+import YoutubeIcon from "../../../public/icons/youtube.svg"
+import PhoneIcon from "../../../public/icons/phone.svg"
+import EmailIcon from "../../../public/icons/email.svg"
+
 
 export default function Topnav() {
 
 
     return (
         <div className={stlyes.TopnavContainer}>
-            <ul>
-                <li className="">info@george-cosmetics.com</li>
-                <li>Phone: +359 123 888 999</li>
-                <li>youtube facebook instagram</li>
-            </ul>
+            <div className={stlyes.TopnavText}>
+                <div className={stlyes.TopnavLeftLinks}>
+
+                    <div>
+                        <a href="mailto: info@george-cosmetics.bg">
+                            <Image src={EmailIcon} alt={"email"} width={35} />
+                            info@george-cosmetics.bg
+                        </a>
+                    </div>
+
+                    <strong> | </strong>
+
+                    <div>
+                        <a href="tel: +359 888 999 777">
+                            <Image src={PhoneIcon} alt={"phone"} width={35} />
+                            +359 888 999 777
+                        </a>
+                    </div>
+
+                </div>
+                <div className={stlyes.TopnavRightLinks}>
+                    <div className="facebook"><Image src={FacebookIcon} alt={"facebook"} width={38} /></div>
+                    <div className="youtube"><Image src={YoutubeIcon} alt={"youtube"} width={40} /></div>
+                    <div className="instagram"><Image src={InstagramIcon} alt={"instagram"} width={35} /></div>
+                </div>
+            </div>
         </div>
     )
 }
