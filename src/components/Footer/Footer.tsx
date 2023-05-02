2c80f4d68fa2d0d1c8b120278573604838100ead
luskoeqk <lusy2017@gmail.com>
2023-05-02 10:10:17 +0300

    foot up

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
                     </div>
                     <div>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d257.2185094129571!2d23.3504891!3d42.7036217!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa85807e4f948f%3A0xd3d8f1988e25505f!2sg.k.%20Hadzhi%20Dimitar%2055%D0%92%20%D0%91%2C%201510%20Hadzhi%20Dimitar%2C%20Sofia!5e1!3m2!1sen!2sbg!4v1683010167324!5m2!1sen!2sbg" width="400" height="300" loading="lazy" style={{ border: "none"}}></iframe>
-                        <h2> кв. "Хаджи Димитър", бл. 55В, вх. Б</h2>
+                        <h2> кв. `&quot;`Хаджи Димитър`&quot;`, бл. 55В, вх. Б</h2>
                     </div>
                 </div>
             </div>
