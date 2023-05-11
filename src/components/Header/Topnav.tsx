11b72d1b04a7072d8830a52a872b425d0d7543d6
luskoeqk <lusy2017@gmail.com>
2023-05-11 12:11:51 +0300

    edits

--- src/components/Header/Topnav.tsx
+++ src/components/Header/Topnav.tsx
@@ -15,30 +15,7 @@ export default function Topnav() {
     return (
         <div className={stlyes.TopnavContainer}>
             <div className={stlyes.TopnavText}>
-                <div className={stlyes.TopnavLeftLinks}>
-
-                    <div>
-                        <a href="mailto: info@george-cosmetics.bg">
-                            <Image src={EmailIcon} alt={"email"} width={35} />
-                            <p>info@george-cosmetics.bg</p>
-                        </a>
-                    </div>
-
-                    <hr className={stlyes.TopnavHR} />
-
-                    <div>
-                        <a href="tel: +359 888 999 777">
-                            <Image src={PhoneIcon} alt={"phone"} width={35} />
-                            <p>+359 888 999 777</p>
-                        </a>
-                    </div>
-
-                </div>
-                <div className={stlyes.TopnavRightLinks}>
-                    <div className="instagram"><Image src={InstagramIcon} alt={"instagram"} width={35} /></div>
-                    <div className="facebook"><Image src={FacebookIcon} alt={"facebook"} width={35} /></div>
-                    <div className="youtube"><Image src={YoutubeIcon} alt={"youtube"} width={35} /></div>
-                </div>
+                <p>За нейно величесто кожата</p>
             </div>
         </div>
     )
