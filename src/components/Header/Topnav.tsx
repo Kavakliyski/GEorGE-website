b19cf74ea080f3510a4fb7edaa60c6066073958c
Kavakliyski <77803201+Kavakliyski@users.noreply.github.com>
2023-07-30 11:41:50 +0300

    dev test2

--- src/components/Header/Topnav.tsx
+++ src/components/Header/Topnav.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "next-i18next";
 import stlyes from "./Topnav.module.scss";
-import axios from "axios";
 
 export default function Topnav() {
     const { t: translate } = useTranslation("header");
