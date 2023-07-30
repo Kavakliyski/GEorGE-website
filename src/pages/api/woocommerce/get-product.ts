1e0352743610464b65ca6fa3acd89e974949188a
Kavakliyski <77803201+Kavakliyski@users.noreply.github.com>
2023-07-30 11:44:48 +0300

    dev test 3

--- src/pages/api/woocommerce/get-product.ts
+++ src/pages/api/woocommerce/get-product.ts
@@ -1,39 +1,39 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+// const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
-const api = new WooCommerceRestApi({
-    url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
-    consumerKey: process.env.WC_CONSUMER_KEY,
-    consumerSecret: process.env.WC_CONSUMER_SECRET,
-    version: "wc/v3",
-});
+// const api = new WooCommerceRestApi({
+//     url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+//     consumerKey: process.env.WC_CONSUMER_KEY,
+//     consumerSecret: process.env.WC_CONSUMER_SECRET,
+//     version: "wc/v3",
+// });
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    const responseData = {
-        success: false,
-        product: [],
-        error: "",
-    };
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse
+// ) {
+//     const responseData = {
+//         success: false,
+//         product: [],
+//         error: "",
+//     };
 
-    try {
-        const { slug } = req.query;
+//     try {
+//         const { slug } = req.query;
 
-        if (!slug) {
-            throw new Error("Product SLUG is missing");
-        }
+//         if (!slug) {
+//             throw new Error("Product SLUG is missing");
+//         }
 
-        const { data } = await api.get(`products?slug=${slug}`);
+//         const { data } = await api.get(`products?slug=${slug}`);
 
-        responseData.success = true;
-        responseData.product = data;
+//         responseData.success = true;
+//         responseData.product = data;
 
-        res.json(responseData);
-    } catch (err: any) {
-        responseData.error = err.message;
-        res.status(500).json(responseData);
-    }
-}
+//         res.json(responseData);
+//     } catch (err: any) {
+//         responseData.error = err.message;
+//         res.status(500).json(responseData);
+//     }
+// }
--- src/pages/api/woocommerce/get-products.ts
+++ src/pages/api/woocommerce/get-products.ts
@@ -1,35 +1,35 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+// const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
-const api = new WooCommerceRestApi({
-    url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
-    consumerKey: process.env.WC_CONSUMER_KEY,
-    consumerSecret: process.env.WC_CONSUMER_SECRET,
-    version: "wc/v3",
-});
+// const api = new WooCommerceRestApi({
+//     url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+//     consumerKey: process.env.WC_CONSUMER_KEY,
+//     consumerSecret: process.env.WC_CONSUMER_SECRET,
+//     version: "wc/v3",
+// });
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    const responseData = {
-        success: false,
-        products: [],
-        error: "",
-    };
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse
+// ) {
+//     const responseData = {
+//         success: false,
+//         products: [],
+//         error: "",
+//     };
 
-    try {
-        const { data } = await api.get("products", {
-            per_page: 50,
-        });
+//     try {
+//         const { data } = await api.get("products", {
+//             per_page: 50,
+//         });
 
-        responseData.success = true;
-        responseData.products = data;
+//         responseData.success = true;
+//         responseData.products = data;
 
-        res.json(responseData);
-    } catch (err: any) {
-        responseData.error = err.message;
-        res.status(500).json(responseData);
-    }
-}
+//         res.json(responseData);
+//     } catch (err: any) {
+//         responseData.error = err.message;
+//         res.status(500).json(responseData);
+//     }
+// }
