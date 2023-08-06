b55fc40e422af077e5f690f562c241150338152a
Kavakliyski <77803201+Kavakliyski@users.noreply.github.com>
2023-08-07 00:02:52 +0300

    feature images

--- src/components/HomepageCompoents/FeatruredProducts/FeaturedProducts.tsx
+++ src/components/HomepageCompoents/FeatruredProducts/FeaturedProducts.tsx
@@ -1,65 +1,55 @@
-import styles from './FeaturedProducts.module.scss';
+import Image from "next/image";
+
+import styles from "./FeaturedProducts.module.scss";
 
 
 export const FeaturedProducts = () => {
-
-
     return (
         <div className={styles.FeaturedProductsWrapper}>
-
             <div className={styles.FeaturedProductsContainer}>
-
                 <div className={styles.Box}>
                     <div className={styles.ImageBox}>
-                        <img alt='test' src="https://images.unsplash.com/photo-1579748138140-ce9ef2c32db1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
+                        <Image width={1000} height={1000} alt="" src={"/static/featureProducts/nach-str-za-vas.jpg"} />
                     </div>
                     <div className={styles.BoxContent}>
                         <div>
-                            <h2>Image Title</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                            </p>
+                            <h2>
+                                Необходимият лукс за Нейно Величество Кожата
+                            </h2>
                         </div>
                     </div>
                 </div>
                 <div className={styles.Box}>
                     <div className={styles.ImageBox}>
-                        <img alt='test' src="https://images.unsplash.com/photo-1579639782539-15cc6c0be63f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
+                        <Image width={1000} height={1000} alt="" src={"/static/featureProducts/nach-str-cream.jpg"} />
                     </div>
                     <div className={styles.BoxContent}>
                         <div>
-                            <h2>Image Title</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                            </p>
+                            <h2>24-каратова красота 7 дни в седмицата</h2>
                         </div>
                     </div>
                 </div>
                 <div className={styles.Box}>
                     <div className={styles.ImageBox}>
-                        <img alt='test' src="https://images.unsplash.com/photo-1603984362497-0a878f607b92?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" />
+                        <Image width={1000} height={1000} alt="" src={"/static/featureProducts/nach-str-serum.jpg"} />
                     </div>
                     <div className={styles.BoxContent}>
                         <div>
-                            <h2>Image Title</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                            </p>
+                            <h2>Ефирност, нежност и деликатност</h2>
                         </div>
                     </div>
                 </div>
                 <div className={styles.Box}>
                     <div className={styles.ImageBox}>
-                        <img alt='test' src="https://images.unsplash.com/photo-1579310962131-aa21f240d986?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80" />
+                        <Image width={1000} height={1000} alt="" src={"/static/featureProducts/nach-stra-mnenia.jpg"} />
                     </div>
                     <div className={styles.BoxContent}>
                         <div>
-                            <h2>Image Title</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                            </p>
+                            <h2>Магия за Вашето щастие</h2>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
-    )
-}
+    );
+};
--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,63 +1,62 @@
-import { GetStaticPropsContext } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { GetStaticPropsContext } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 
+import styles from "@/styles/pages/reviews.module.scss";
 
 export default function reviews() {
-
-
     return (
         <>
             <Head>
                 <title>GEorGE • Cosmetic • Reviews</title>
             </Head>
 
-
-            <div>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
-                <h1>reviews</h1>
+            <div className={styles.reviewsWrapper}>
+                <div className={styles.reviewsContainer}>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                    <h1>reviews</h1>
+                </div>
             </div>
         </>
-    )
+    );
 }
-
 
 export async function getStaticProps(context: GetStaticPropsContext) {
     const { locale } = context;
 
     if (!locale) {
-        throw new Error('Locale is not available in context');
+        throw new Error("Locale is not available in context");
     }
 
     return {
         props: {
-            ... (await serverSideTranslations(locale, ['common', 'header']))
-        }
-    }
+            ...(await serverSideTranslations(locale, ["common", "header"])),
+        },
+    };
 }
