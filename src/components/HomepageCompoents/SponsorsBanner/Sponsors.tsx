0721f4aeeee098d2671d8a36ece8a62b4240b602
Kavakliyski <77803201+Kavakliyski@users.noreply.github.com>
2023-08-13 00:00:01 +0300

    translations

--- src/components/HomepageCompoents/SponsorsBanner/Sponsors.tsx
+++ src/components/HomepageCompoents/SponsorsBanner/Sponsors.tsx
@@ -2,5 +2,9 @@
 import styles from "./Sonsors.module.scss";
 
 export default function Sponsors() {
-    return <div className={styles.SponsorsWrapper}>Sponsors</div>;
+    return (
+        <div className={styles.SponsorsWrapper}>
+            <div>Партньори</div>
+        </div>
+    );
 }
