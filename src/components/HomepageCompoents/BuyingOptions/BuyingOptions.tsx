4adf56e8136c0e644b2f63c27b6c63bdeeff1e37
Lyudmil <lusy2017@gmail.com>
2023-08-26 12:28:53 +0300

    stylings and seo

--- src/components/HomepageCompoents/BuyingOptions/BuyingOptions.tsx
+++ src/components/HomepageCompoents/BuyingOptions/BuyingOptions.tsx
@@ -1,15 +1,17 @@
 import styles from "./BuyingOptions.module.scss";
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
 
 type BoxProps = {
     text: string;
     phone?: string;
+    href?: string;
 };
 
-function Box({ text, phone }: BoxProps) {
+function Box({ text, phone, href }: BoxProps) {
     return (
         <div className={styles.Box}>
-            <div>{text}</div>
+            {href ? <Link href={href}>{text}</Link> : <div>{text}</div>}
             <div>
                 <a href={`tel:${phone}`}>{phone}</a>
             </div>
@@ -24,10 +26,10 @@ export default function BuyingOptions() {
         <div className={styles.BuyingOptionsWrapper}>
             <h2>{translate("purchase_options")}</h2>
             <div className={styles.gridContainer}>
-                <Box text={translate('addr')} />
+                <Box text={translate("addr")} />
                 <Box text={translate("buysponsors")} />
                 <Box text={translate("orderbyphone")} phone={"0876 570 471"} />
-                <Box text={translate("orderonline")} />
+                <Box text={translate("orderonline")} href={"/shop"} />
             </div>
         </div>
     );
