966b9a03cff46d15042d5b3c9d20b35df729c269
Kavakliyski <77803201+Kavakliyski@users.noreply.github.com>
2023-10-03 23:23:25 +0300

    reloadOnPrerender about test

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -86,5 +86,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
             ])),
             posts: data.posts.edges[0].node.content,
         },
+        reloadOnPrerender: true
     };
 }
