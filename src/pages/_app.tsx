51ec0709c88e424bfab05c84a1368e2453d95e91
Lyudmil <lusy2017@gmail.com>
2023-10-08 22:08:48 +0300

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
+import Script from "next/script";
 
 // styles
 import "@/styles/globals.scss";
@@ -47,7 +48,42 @@ function App({ Component, pageProps }: AppProps) {
                     content="Брандът GEorGE е създаден въз основа на иновативните технологии в световната козметична индустрия. Смело можем да твърдим, че ние сме революционери в разработването на собствени натурални продукти с колоидно злато, колоидно сребро и хидролизиран хиалурон, които от своя страна действат на дълбоко клетъчно ниво и подпомагат регенерацията на кожата."
                 />
                 <meta name="keywords" content="keyword1, keyword2, keyword3" />
+                <Script>
+                    {/* Google tag Manager */}
+                    {`
+                    (function(w,d,s,l,i){
+                        w[l]=w[l]||[];
+                        w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+                        var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                        j.async=true;
+                        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                        f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-PC3ZHPZB');
+                `}
+                </Script>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-EY5V50HHTF"
+                    strategy="lazyOnload"
+                />
+                <Script strategy="lazyOnload">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-EY5V50HHTF');
+                `}
+                </Script>
             </Head>
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-PC3ZHPZB"
+                    height="0"
+                    width="0"
+                    style={{ display: "none", visibility: "hidden" }}
+                ></iframe>
+            </noscript>
             <InternalizationContextProvider>
                 <CartContextProvider>
                     <Topnav />
